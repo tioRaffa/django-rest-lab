81510444c3de0ec81859a3cb01f8ad9b15daa541
tioRaffa <rafaelmuniz200@gmail.com>
2025-08-27 10:40:12 -0300

    Adiciona métodos de filtragem e manipulação para a API de livros, incluindo GET e POST, além de implementar o método PATCH para categorias e livros.

--- book/views.py
+++ book/views.py
@@ -21,7 +21,6 @@ class CategoriesAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-
 class CategoryAPIView(APIView):
     def get_object(self, pk):
         obj = get_object_or_404(CategoryModel, pk=pk)
@@ -35,6 +34,52 @@ class CategoryAPIView(APIView):
     def patch(self, request, pk):
         category = self.get_object(pk=pk)
         serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class BooksAPIView(APIView):
+    def get(self, request):
+        books = BooksModel.objects.all()
+
+        # FILTRO
+        category_id = request.query_params.get('category')
+        if category_id:
+            books = books.filter(category_id=category_id)
+
+        title = request.query_params.get('title')
+        if title:
+            books = books.filter(title__icontains=title)
+
+        author = request.query_params.get('autor')
+        if author:
+            books = books.filter(author__icontains=author)
+        # ----
+
+        serializer = BooksSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = BooksSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BookAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(BooksModel, pk=pk)
+    
+    def get(self, request, pk):
+        book = self.get_object(pk=pk)
+        serializer = BooksSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def patch(self, request, pk):
+        book = self.get_object(pk=pk)
+        serializer = BooksSerializer(book, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
