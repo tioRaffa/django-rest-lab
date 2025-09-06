aacff1c16e64db2a1d934951208778acf74e05a0
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-09-06 10:38:21 -0300

    feat(movies_app): implement search functionality for movies by name in admin view

--- movies_app/api/search_name.py
+++ movies_app/api/search_name.py
@@ -1,2 +1,49 @@
 import requests
+from pprint import pprint
+from decouple import config
 
+def search_movie_by_name(movie_name: str):
+    api_key = config('TMDB_API_KEY')
+    if not api_key:
+        print('Chave API não encontrada no arquivo .env')
+        return None
+    
+    if not movie_name:
+        print('Nome do Filme não informado')
+        return None
+        
+        
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_name}"
+    try:
+        response = requests.get(url=url)
+        data = response.json()
+        if response.status_code != 200:
+           print(f"Erro na API: {data.get('status_message', 'Error desconhecido')}")    
+           return None
+       
+        list_films = data.get('results', [])
+        if not list_films:
+            print('error.. nenhum filme encontrado com este nome')
+            return None
+        
+        filter_films = []
+        for film in list_films:
+            info = {
+                'movie_id': film.get('id'),
+                'backdrop_path': film.get('backdrop_path'),
+                'title': film.get('original_title'),
+                'genre_ids': film.get('genre_ids'),
+                'overview': film.get('overview')
+            }
+            filter_films.append(info)
+        
+        return filter_films
+
+    except requests.exceptions.RequestException as e:
+       print(f'Erro na requisição: {e}')
+       return None
+    
+        
+if __name__ == '__main__':
+    info = search_movie_by_name('batman')
+    pprint(info)
---
+++ movies_app/views/admin_search_view.py
@@ -0,0 +1,26 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from movies_app.api.search_name import search_movie_by_name
+
+
+class SearchMovieByNameAdminAPIView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    
+    def get(self, request):
+        title = request.query_params.get('titulo')
+        if not title:
+            return Response({
+                'error': 'Informe o parametro TITULO.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+            
+        movies_data = search_movie_by_name(title)
+        if movies_data:
+            return Response(
+                movies_data, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"detail": "Nenhum filme encontrado com o título informado."},
+                status=status.HTTP_404_NOT_FOUND
+            )
